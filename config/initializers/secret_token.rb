9bea39ba8a129612c3554a99cd099591a2166b4b
Scott Snare <ssnare@hdvp.com>
2011-03-11 18:10:01 -0800

    initial committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adkata::Application.config.secret_token = 'f5f9aa6b12f688ba4a66e4e701fbf6e48d1a9e308d76846ce58f062074d3453914bd96bf88ad9d045b89c8964cadabab88873b59b9d9446370953febf2d6fd2c'
